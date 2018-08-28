2903439eea4a4c83f1732e4e689b75a024e2ed71
Unknown <9443295+ElementalCrisis@users.noreply.github.com>
2018-08-28 14:48:11 -0700

    Initial upload.

---
+++ assets/js/shoko.js
@@ -0,0 +1,27 @@
+randomBanner = Math.floor((Math.random() * 4) + 1);
+
+let header = getCSSRule('.banner-container');
+header.style.background = ' url("../images/banners/Banner-' + randomBanner + '.jpg") 50% 20%';
+header.style.backgroundSize = 'cover';
+
+console.log(randomBanner);
+
+function getCSSRule(ruleName) {
+
+    ruleName = ruleName.toLowerCase();
+    let result = null;
+    let find = Array.prototype.find;
+
+    find.call(document.styleSheets, styleSheet => {
+        result = find.call(styleSheet.cssRules, cssRule => {
+            return cssRule instanceof CSSStyleRule
+                && cssRule.selectorText.toLowerCase() == ruleName;
+        });
+
+        return result != null
+
+    });
+
+    return result;
+
+}
