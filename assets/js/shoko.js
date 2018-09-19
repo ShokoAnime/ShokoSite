e8ef6983a2e7ebc9c1765b96e872dbefaa896454
Elemental Crisis <9443295+ElementalCrisis@users.noreply.github.com>
2018-09-19 03:09:53 -0700

    Update downloads, add lightbox and add random plugin code.

--- assets/js/shoko.js
+++ assets/js/shoko.js
@@ -1,6 +1,48 @@
+// Global Variables
+
 let randomNumber;
 let previousBanner = sessionStorage.getItem("bannerID");
-let randomBanner = getRandomBanner(randomNumber);
+let randomBanner = getRandomBanner();
+let randomPlugin = getRandomPlugin();
+
+let bannerInfo = [
+    {
+        name: "My Anime 3",
+        imageURL: "assets/images/programs-plugins/my-anime-3/My-Anime-3-StreamedMP-Collection-List.jpg",
+        imageName: "My Anime 3 - StreamedMP - Collection List",
+        info: "Shoko, previously known as JMM (Japanese Media Manager) is an anime cataloging program designed to " +
+            "automate the cataloging of your anime collection regardless of the size and number of files in your " +
+            "collection, it’s even able to connect with some of the most popular media center programs out there. "
+    },
+    {
+        name: "Shoko On Plex",
+        imageURL: "assets/images/programs-plugins/shoko-on-plex/Shoko-On-Plex-Poster-Listing.jpg",
+        imageName: "Shoko On Plex - Poster Listing",
+        info: "Shoko, previously known as JMM (Japanese Media Manager) is an anime cataloging program designed to " +
+            "automate the cataloging of your anime collection regardless of the size and number of files in your " +
+            "collection, it’s even able to connect with some of the most popular media center programs out there. "
+    },
+    {
+        name: "Nakamori",
+        imageURL: "assets/images/programs-plugins/nakamori/Nakamori-Coverflow.jpg",
+        imageName: "Nakamori - Cover Flow",
+        info: "Nakamori takes the versatility of Kodi and combines it with the power of Shoko to bring your anime " +
+            "collection to life. Unlike other anime plugin currently available for Kodi, your entire " +
+            "collection’s metadata is automatically managed and made available within Kodi providing you with an " +
+            "rich user experience. But that’s not all, Nakamori also handles the syncing of your episode and series " +
+            "watched states and series votes automatically allowing you to keep your collection current and up-to-date."
+    },
+    {
+        name: "Shoko Metadata",
+        imageURL: "assets/images/programs-plugins/shoko-metadata/Shoko-Metadata-Series-Listing.jpg",
+        imageName: "Shoko Metadata - Series Listing",
+        info: "Shoko, previously known as JMM (Japanese Media Manager) is an anime cataloging program designed to " +
+            "automate the cataloging of your anime collection regardless of the size and number of files in your " +
+            "collection, it’s even able to connect with some of the most popular media center programs out there. "
+    }
+];
+
+// Random Banner
 
 let header = getCSSRule('.banner-container');
 header.style.background = ' url("../images/banners/Banner-' + randomBanner + '.jpg") 50% 20%';
@@ -12,7 +54,7 @@ console.log("Previous Random: " + previousBanner);
 while (parseInt(randomBanner, 10) === parseInt(previousBanner, 10)) {
 
     if (parseInt(randomBanner, 10) === parseInt(previousBanner, 10)) {
-        randomBanner = getRandomBanner(randomNumber);
+        randomBanner = getRandomBanner();
         console.log("New Random: " + randomBanner);
     }
 
@@ -20,8 +62,24 @@ while (parseInt(randomBanner, 10) === parseInt(previousBanner, 10)) {
 
 sessionStorage.setItem("bannerID", randomBanner);
 
+// Random Plugin
+
+console.log("Random Plugin: " + randomPlugin);
+document.getElementById("plugin-name").innerHTML = bannerInfo[randomPlugin].name;
+document.getElementById("plugin-info").innerHTML = bannerInfo[randomPlugin].info;
+document.getElementById("plugin-image").setAttribute("src", bannerInfo[randomPlugin].imageURL);
+document.getElementById("plugin-image-url").setAttribute('href', bannerInfo[randomPlugin].imageURL);
+document.getElementById("plugin-image-url").setAttribute('data-title', bannerInfo[randomPlugin].imageName);
+
 function getRandomBanner() {
+
     randomNumber = Math.floor((Math.random() * 10) + 1);
+    return randomNumber;
+}
+
+function getRandomPlugin() {
+
+    randomNumber = Math.floor((Math.random() * 4));
     return randomNumber;
 }
 
