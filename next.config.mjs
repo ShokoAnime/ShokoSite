d822cbdc62be9833bb69b4c86b34124bbf415f1d
duehoa1211 <duehoa1211@gmail.com>
2024-02-06 13:12:29 +0700

    Init shoko site as nextjs project

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import withMDX from '@next/mdx';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+};
+
+export default withMDX()(nextConfig);
