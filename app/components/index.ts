d36f2aa9bc772d761f691e0aa79f8a0692142a5a
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-05-09 01:10:04 -0700

    Hopefully fix home directory name.

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,6 @@
-export { HistorySection } from './about';
-export { Button, Icon, Loading } from './common';
-export { ContributorsGroup, UserCard } from './contributors';
-export { DownloadCallout, DownloadGrid, DownloadNavTabs, DownloadSingle, DownloadsIndex } from './downloads';
-export { Benefit, Hero, InfoGroups, NewsPost } from './home';
-export { Footer, Header, PageBanner, PageNotFound, ScrollWrapper } from './layout';
+export * from './about';
+export * from './common';
+export * from './contributors';
+export * from './downloads';
+export * from './home';
+export * from './layout';
