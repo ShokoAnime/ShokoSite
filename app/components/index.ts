c72123c459492c1f80b79f36e955bb225ae4d1f9
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-05-09 00:53:55 -0700

    Update exports.

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,6 @@
-export * from './about';
-export * from './common';
-export * from './contributors';
-export * from './downloads';
-export * from './home';
-export * from './layout';
+export { HistorySection } from './about';
+export { Button, Icon, Loading } from './common';
+export { ContributorsGroup, UserCard } from './contributors';
+export { DownloadCallout, DownloadGrid, DownloadNavTabs, DownloadSingle, DownloadsIndex } from './downloads';
+export { Benefit, Hero, InfoGroups, NewsPost } from './home';
+export { Footer, Header, PageBanner, PageNotFound, ScrollWrapper } from './layout';
