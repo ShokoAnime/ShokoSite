b8934485ff464bb84f16e8ef0fbd7676d121553f
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-05-15 01:07:05 -0700

    Fix Screen flash on theme load.

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse, useRouteError } from '@remix-run/react';
 import { Providers } from '~/context/Providers';
 import '~/css/tailwind.css';
@@ -32,8 +32,15 @@ export function ErrorBoundary() {
 export function Layout({ children }: { children: React.ReactNode }) {
   const { theme } = getThemeData();
 
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    setTimeout(function() {
+      htmlElement.classList.remove('hidden');
+    }, 200);
+  }, []);
+
   return (
-    <html lang="en" className={theme}>
+    <html lang="en" className={`hidden ${theme}`}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
