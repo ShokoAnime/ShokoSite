ad5ba5e97308bef74373aca6d77eaa35e4f257e9
duehoa1211 <duehoa1211@gmail.com>
2024-05-26 01:19:44 +0700

    Mdx sample

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="@remix-run/node" />
+/// <reference types="vite/client" />
+
+declare module "*.mdx" {
+  let MDXComponent: (props: any) => JSX.Element;
+  export default MDXComponent;
+}
