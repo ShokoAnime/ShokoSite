3294042af1acf666ba3d37b80160a16579c084fc
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-05-26 02:03:17 -0700

    Add Date Conversion Helper Function.

--- app/helpers/utils.tsx
+++ app/helpers/utils.tsx
@@ -10,3 +10,31 @@ export const convertNameToUrl = (path: string) => {
     .replace(/ /g, '-')
     .toLowerCase();
 };
+
+export const convertDate = (dateString: string) => {
+  const getDaySuffix = (day: number) => {
+    if (day > 3 && day < 21) return 'th';
+    switch (day % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+  };
+
+  const dateParts = dateString.split('-');
+  const year = parseInt(dateParts[0], 10);
+  const monthIndex = parseInt(dateParts[1], 10) - 1;
+  const day = parseInt(dateParts[2], 10);
+
+  const date = new Date(year, monthIndex, day);
+  const options: Intl.DateTimeFormatOptions = { month: 'long' };
+  const month = date.toLocaleDateString('en-US', options);
+  const daySuffix = getDaySuffix(day);
+
+  return `${month} ${day}${daySuffix}, ${year}`;
+};
