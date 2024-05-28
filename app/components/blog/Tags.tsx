43d0e3bbe1b7e4fe888bbd3b32d214cc13528bdf
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-05-27 22:22:16 -0700

    WIP - Generate Tag Listing.

--- app/components/blog/Tags.tsx
+++ app/components/blog/Tags.tsx
@@ -1,41 +1,59 @@
+import { useEffect, useState } from 'react';
+import cx from 'classnames';
+import { markdownList } from '~/helpers/markdown-list';
+import { BlogPostProps } from '~/types/BlogTypes';
 import HighLightHeader from '~/components/blog/HighLightHeader';
-import { useState } from 'react';
 
 type TagItemProps = {
   name: string;
-  total: number;
-  isChosen: boolean;
-}
+  count: number;
+};
 
-const tags: TagItemProps[] = [
-  { name: 'Shoko', total: 10, isChosen: true },
-  { name: 'Anime', total: 5, isChosen: false },
-  { name: 'Manga', total: 3, isChosen: false },
-  { name: 'Light Novel', total: 2, isChosen: false },
-  { name: 'Visual Novel', total: 1, isChosen: false },
-];
+const Tags: React.FC = () => {
+  const [tagList, setTagList] = useState<TagItemProps[]>([]);
 
-function TagItem({ isChosen, total, name }: TagItemProps) {
-  const [isChecked, setIsChecked] = useState(isChosen);
-  return <div className="mb-0 flex w-full items-center justify-between border-b py-3 font-semibold">
-    <span>{name}</span>
-    <span className="flex gap-x-2">
-      <span>({total})</span>
-      <input type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} />
-    </span>
-  </div>;
-}
+  useEffect(() => {
+    const fetchDataAndProcessTags = async () => {
+      const dataList = markdownList('blog');
+      const tagCountMap: TagItemProps[] = [];
 
+      dataList.forEach((item: BlogPostProps) => {
+        item.frontmatter.tags.forEach((tag: string) => {
+          const existingTag = tagCountMap.find((t) => t.name === tag);
+          if (existingTag) {
+            existingTag.count += 1;
+          } else {
+            tagCountMap.push({ name: tag, count: 1 });
+          }
+        });
+      });
 
-function Tags() {
-  return <div className="flex w-full flex-col">
-    <HighLightHeader title="Tags" />
-    <div>
-      {tags.map((tag) => (
-        <TagItem key={tag.name} {...tag} />
-      ))}
+      tagCountMap.sort((a, b) => a.name.localeCompare(b.name));
+      setTagList(tagCountMap);
+    };
+
+    fetchDataAndProcessTags();
+  }, []);
+
+  return (
+    <div className="mb-0 flex flex-col w-full justify-between gap-y-6 font-semibold">
+      <HighLightHeader title="Tags" />
+      <div className="flex flex-col gap-y-2">
+        {tagList.map((tag, index) => (
+          <div
+            key={tag.name}
+            className={cx(
+              'flex justify-between pb-2',
+              tagList.length !== index + 1 && 'border-b border-shoko-border',
+            )}
+          >
+            <div>{tag.name}</div>
+            <div>({tag.count})</div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>;
-}
+  );
+};
 
 export default Tags;
