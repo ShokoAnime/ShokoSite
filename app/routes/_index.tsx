d8df92025fa00def3ac7f72797ccbade9c55cdbf
hidden4003 <hidden4003@users.noreply.github.com>
2024-06-24 21:05:53 +0300

    Fix file case on Windows Fix blog post footer collapsing

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,7 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <div className="h-screen">
       <Hero />
       <Benefits />
       <InfoGroups />
