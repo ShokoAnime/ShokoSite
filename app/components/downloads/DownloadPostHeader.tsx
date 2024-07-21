c46e4606b2e2f58efc4630cdafbbe42f8451d804
duehoa1211 <duehoa1211@gmail.com>
2024-07-21 17:17:23 +0700

    initial responsive for download pages

--- app/components/downloads/DownloadPostHeader.tsx
+++ app/components/downloads/DownloadPostHeader.tsx
@@ -26,7 +26,7 @@ const DownloadPostHeader = ({ title, resources }: DownloadHeaderProps) => {
   return (
     <div className="flex items-center justify-between">
       <SectionHeader title={title} type="h4" />
-      <div className="flex gap-x-2">
+      <div className="hidden md:flex gap-x-2">
         {resources.map((resource) => (
           <a key={resource.name} className="text-sm" href={resource.url} target="_blank" rel="noreferrer">
             <Button className="px-3 py-2" buttonType="outline">
