d8d8ab8db8fdbddfad47731d9f81a44aee3fc085
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com.>
2024-07-31 20:53:36 -0700

    Rework Index Responsive/Mobile.

--- app/components/home/InfoGroups.data.tsx
+++ app/components/home/InfoGroups.data.tsx
@@ -1,6 +1,6 @@
 const mediaPlayers = [
   { name: 'Nakamori | Shokodi', mediaPlayer: 'Kodi Plugin', image: '/images/home/kodi.svg' },
-  { name: 'Shoko Metadata | ShokoRelay', mediaPlayer: 'Plex Agent & Scanner', image: '/images/home/plex.svg' },
+  { name: 'ShokoRelay', mediaPlayer: 'Plex Agent & Scanner', image: '/images/home/plex.svg' },
   { name: 'My Anime 3', mediaPlayer: 'MediaPortal 1 Plugin', image: '/images/home/media-portal.svg' },
   { name: 'Shokofin', mediaPlayer: 'Jellyfin Plugin', image: '/images/home/jellyfin.svg' },
 ];
@@ -18,7 +18,7 @@ const InfoGroupDetails = [
           or file renaming, ensuring your collection is well-organized and accessible across multiple devices with media
           player plugins.
         </div>
-        <div className="text-shoko-highlight font-medium">
+        <div className="font-medium text-shoko-highlight">
           Please be aware that Shoko does not offer any means to download files, stream files from streaming sites, or
           access files that are not part of your personal collection.
         </div>
@@ -58,14 +58,14 @@ const InfoGroupDetails = [
       <>
         <div>
           Shoko offers more than just automated collection management. It supports a wide range of media players,
-          allowing you to watch your collection on thefk device of your choice, wherever you’d like.
+          allowing you to watch your collection on the device of your choice, wherever you’d like.
         </div>
-        <div className="mt-4 grid grid-cols-2 grid-rows-2 gap-y-8">
+        <div className="mt-4 grid grid-cols-1 grid-rows-2 gap-y-8 md:grid-cols-2">
           {mediaPlayers.map((data) => (
-            <div key={data.name} className="flex items-center gap-x-4">
+            <div key={data.name} className="ml-8 flex w-fit justify-center gap-x-4">
               <img width={60} height={60} src={data.image} alt={data.name} />
               <div className="flex flex-col">
-                <div className="text-shoko-text-header font-header text-lg font-semibold">
+                <div className="font-header text-lg font-semibold text-shoko-text-header">
                   {data.name}
                 </div>
                 <div>{data.mediaPlayer}</div>
--- app/types/layout.ts
+++ app/types/layout.ts
@@ -10,10 +10,15 @@ export type InternalLinksProps = {
   title: string;
   route: string;
   isActive?: boolean;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 };
 
 export type ExternalLinksProps = {
   title: string;
   url: string;
   icon?: React.ReactNode;
+};
+
+export type MobileMenuProps = {
+  setShowMobileMenu: (val: boolean) => void;
 };
