2d3c35a0d12809f024d805e89ef53fd53d3cc02b
duehoa1211 <duehoa1211@gmail.com>
2024-09-01 20:51:34 +0700

    Update download page responsive

--- app/components/downloads/IndexCard.tsx
+++ app/components/downloads/IndexCard.tsx
@@ -1,60 +1,72 @@
-import { mdiLayersTriple, mdiTelevision, mdiThemeLightDark, mdiTools, mdiWeatherSunset } from '@mdi/js';
-import { Link } from '@remix-run/react';
+import {
+  mdiLayersTriple,
+  mdiTelevision,
+  mdiThemeLightDark,
+  mdiTools,
+  mdiWeatherSunset,
+} from "@mdi/js";
+import { Link } from "@remix-run/react";
 
-import { DownloadCategories } from '~/types/downloads';
-import Icon from '~/components/common/Icon';
+import { DownloadCategories } from "~/types/downloads";
+import Icon from "~/components/common/Icon";
 
 const DownloadIndexCard = () => {
   const downloadData = [
     {
-      title: 'Shoko Server',
-      description: 'The main program, required for everything else to properly work. ',
+      title: "Shoko Server",
+      description:
+        "The main program, required for everything else to properly work. ",
       icon: mdiLayersTriple,
-      link: '/downloads/shoko-server',
+      link: "/downloads/shoko-server",
     },
     {
-      title: 'Media Player Plugins',
-      description: 'Plugins to get Shoko working with various media players programs.',
+      title: "Media Player Plugins",
+      description:
+        "Plugins to get Shoko working with various media players programs.",
       icon: mdiTelevision,
-      link: '/downloads/media-player-plugins',
+      link: "/downloads/media-player-plugins",
     },
     {
-      title: 'Web UI Themes',
-      description: 'Browse our collection of user submitted themes to enhance the Web UI. ',
+      title: "Web UI Themes",
+      description:
+        "Browse our collection of user submitted themes to enhance the Web UI. ",
       icon: mdiThemeLightDark,
-      link: '/downloads/webui-themes',
+      link: "/downloads/webui-themes",
     },
     {
-      title: 'Renamer Plugins',
-      description: 'Plugins to replace Shoko’s built-in renamer functionality',
+      title: "Renamer Plugins",
+      description: "Plugins to replace Shoko’s built-in renamer functionality",
       icon: mdiTools,
-      link: '/downloads/renamer-plugins',
+      link: "/downloads/renamer-plugins",
     },
     {
-      title: 'Legacy Apps',
-      description: 'Deprecated apps that are listed for archival purposes only.',
+      title: "Legacy Apps",
+      description:
+        "Deprecated apps that are listed for archival purposes only.",
       icon: mdiWeatherSunset,
-      link: '/downloads/legacy',
+      link: "/downloads/legacy",
     },
   ];
 
-  const SingleDownload = ({ title, description, icon, link }: DownloadCategories) => {
+  const SingleDownload = ({
+    title,
+    description,
+    icon,
+    link,
+  }: DownloadCategories) => {
     return (
-      <Link
-        key={title}
-        to={link}
-      >
-        <div className="bg-shoko-bg border-shoko-border border-b-shoko-link hover:bg-shoko-link hover:text-shoko-text-alt group flex w-full max-w-[28.125rem] flex-col items-center gap-x-6 gap-y-4 rounded-lg border border-b-2 p-4 transition-colors duration-500 ease-in-out">
+      <Link key={title} to={link}>
+        <div className="group flex w-full max-w-[28.125rem] flex-col items-center gap-x-6 gap-y-4 rounded-lg border border-b-2 border-shoko-border border-b-shoko-link bg-shoko-bg p-4 transition-colors duration-500 ease-in-out hover:bg-shoko-link hover:text-shoko-text-alt">
           <Icon
-            className="text-shoko-text-header group-hover:text-shoko-text-alt transition-colors duration-500 ease-in-out"
+            className="text-shoko-text-header transition-colors duration-500 ease-in-out group-hover:text-shoko-text-alt"
             icon={icon}
             size={100}
           />
           <div className="flex w-full max-w-[228px] flex-col gap-y-2">
-            <div className="font-header text-shoko-text-header group-hover:text-shoko-text-alt flex justify-center text-xl font-semibold transition-colors duration-500 ease-in-out">
+            <div className="flex justify-center font-header text-xl font-semibold text-shoko-text-header transition-colors duration-500 ease-in-out group-hover:text-shoko-text-alt">
               {title}
             </div>
-            <div className="font-body flex text-center text-base">
+            <div className="flex text-center font-body text-base">
               {description}
             </div>
           </div>
@@ -64,8 +76,10 @@ const DownloadIndexCard = () => {
   };
 
   return (
-    <div className="flex flex-wrap gap-8">
-      {downloadData.map((download) => <SingleDownload key={download.title} {...download} />)}
+    <div className="flex flex-wrap justify-center gap-8">
+      {downloadData.map((download) => (
+        <SingleDownload key={download.title} {...download} />
+      ))}
     </div>
   );
 };
--- app/routes/downloads/downloadsIndex.tsx
+++ app/routes/downloads/downloadsIndex.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { DownloadsIndexProps } from '~/types/downloads';
-import { getDownloadsCount } from '~/helpers/markdown';
-import PageBanner from '~/components/layout/PageBanner';
-import SectionHeader from '~/components/common/SectionHeader';
-import DownloadIndexCard from '~/components/downloads/IndexCard';
+import { DownloadsIndexProps } from "~/types/downloads";
+import { getDownloadsCount } from "~/helpers/markdown";
+import PageBanner from "~/components/layout/PageBanner";
+import SectionHeader from "~/components/common/SectionHeader";
+import DownloadIndexCard from "~/components/downloads/IndexCard";
+import { useMobile } from "~/hooks/useMobile";
 
 function DownloadsIndex() {
   const [downloadCount, setDownloadCount] = useState<DownloadsIndexProps>();
@@ -19,18 +20,18 @@ function DownloadsIndex() {
 
   const headerSubtitle = (
     <>
-      <div className="flex gap-x-2">
+      <div className="hidden gap-x-2 lg:flex">
         <div className="flex gap-x-2">
           <div className="text-shoko-highlight">{downloadCount?.programs}</div>
           <div>Program / Plugins</div>
         </div>
         <div>|</div>
-        <div className="flex gap-x-2">
+        <div className="hidden gap-x-2 lg:flex">
           <div className="text-shoko-highlight">{downloadCount?.themes}</div>
           <div>Web UI Themes</div>
         </div>
         <div>|</div>
-        <div className="flex gap-x-2">
+        <div className="hidden gap-x-2 lg:flex">
           <div className="text-shoko-highlight">{downloadCount?.renamers}</div>
           <div>Renamer Plugin</div>
         </div>
@@ -44,8 +45,14 @@ function DownloadsIndex() {
         title="DownloadsIndex"
         description="Browse through selection of programs, plugins, Web UI Themes and other tools available in the Shoko Suite."
       />
-      <div className="text-shoko-text-header mx-auto flex h-full min-h-[calc(100vh-557px)] max-w-[1440px] flex-col gap-x-2 gap-y-16 py-16">
-        <SectionHeader title="Download Categories" subtitle={headerSubtitle} type="h2" center={true} opacity={100} />
+      <div className="mx-auto flex h-full min-h-[calc(100vh-557px)] max-w-[1440px] flex-col gap-x-2 gap-y-16 py-16 text-shoko-text-header">
+        <SectionHeader
+          title="Download Categories"
+          subtitle={headerSubtitle}
+          type="h2"
+          center={true}
+          opacity={100}
+        />
         <DownloadIndexCard />
       </div>
     </>
