c1ab14a142313dd6db162282bc5f738eddf7f384
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com.>
2024-09-21 00:42:07 -0700

    Potential Cloudflare Fix.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from '@remix-run/node';
+import type { MetaFunction } from '@remix-run/cloudflare';
 import Hero from '~/components/home/Hero';
 import Features from '~/components/home/Features';
 import Divider from '~/components/layout/Divider';
