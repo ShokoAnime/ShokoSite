fdda97de64cebc02d60e3786a1a289efde60d65e
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com.>
2024-09-24 19:20:39 -0700

    Fix Build.

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,10 @@ import remarkGfm from 'remark-gfm';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['highlight.js'],
+      external: [
+        'highlight.js',
+        'highlight.js/styles/github-dark.css',
+      ],
     },
   },
   plugins: [
