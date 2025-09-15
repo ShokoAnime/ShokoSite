b53fbcc3e04e2fbee10c169b1ca004adb7a10f46
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-09-14 17:06:44 -0700

    Just return github response and set cache headers

--- app/routes/api.getGitHubRelease.$owner.$repository.tsx
+++ app/routes/api.getGitHubRelease.$owner.$repository.tsx
@@ -1,39 +1,26 @@
 import { LoaderFunction, json } from "@remix-run/cloudflare";
-import { GitHubRelease } from "../types/githubRelease";
 
-interface CacheEntry {
-    release: GitHubRelease;
-    timestamp: number;
-}
-
-const releaseCache: { [k: string]: CacheEntry } = {};
-
-const CACHE_DURATION_MS = 24 * 60 * 60 * 1000; // 24 Hours
+const CACHE_DURATION = 60 * 60; // 1 hour
 
 export const loader: LoaderFunction = async ({ params }) => {
-    const repository: string = `${params.owner}/${params.repository}`;
-    const now = Date.now();
-    const key = repository.toLowerCase();
-    const cachedEntry = releaseCache[key];
-
-    if (cachedEntry && now - cachedEntry.timestamp < CACHE_DURATION_MS)
-        return cachedEntry.release;
-
-    const url = `https://api.github.com/repos/${key}/releases/latest`;
-    const reqOpts = {
-        headers: {
-            'Accept': 'application/vnd.github+json',
-            'X-GitHub-Api-Version': '2022-11-28',
-        }
-    };
+    const repository: string = `${params.owner}/${params.repository}`.toLowerCase();
     try {
-        const response = await fetch(url, reqOpts);
-        if (!response.ok)
+        const githubResponse = await fetch(`https://api.github.com/repos/${repository}/releases/latest`, {
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'X-GitHub-Api-Version': '2022-11-28',
+            }
+        });
+        if (!githubResponse.ok)
             return new Response('Not Found', { status: 404 });
 
-        const release: GitHubRelease = await response.json();
-        releaseCache[key] = { release: release, timestamp: now };
-        return json(release);
+        const response = json(await githubResponse.json());
+        const cacheControl = githubResponse.headers.get('Cache-Control')?.replace(/s\-maxage=\d+/, `s-maxage=${CACHE_DURATION}`) ?? `s-maxage=${CACHE_DURATION}`;
+        const lastModified = githubResponse.headers.get('Last-Modified');
+        response.headers.set('Cache-Control', cacheControl);
+        if (lastModified)
+            response.headers.set('Last-Modified', lastModified);
+        return response;
     } catch (error) {
         console.error("Error fetching github release: ", error);
         return new Response('Not Found', { status: 404 });
