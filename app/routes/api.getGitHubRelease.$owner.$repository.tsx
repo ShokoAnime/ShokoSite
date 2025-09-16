1e7b017bc336a7f87d6889d93749a35ed60f928f
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-09-16 00:39:35 -0700

    Add owner whitelist

--- app/routes/api.getGitHubRelease.$owner.$repository.tsx
+++ app/routes/api.getGitHubRelease.$owner.$repository.tsx
@@ -1,11 +1,16 @@
 import { LoaderFunction, json } from "@remix-run/cloudflare";
 
 const CACHE_DURATION = 60 * 60; // 1 hour
+const OWNERS = ['ShokoAnime', 'Cazzar', 'Mik1ll', 'bigretromike', 'natyusha',];
 
 export const loader: LoaderFunction = async ({ params }) => {
-    const repository: string = `${params.owner}/${params.repository}`.toLowerCase();
+    const owner = params.owner?.toLowerCase();
+    const repository = params.repository?.toLowerCase();
+    if (OWNERS.every(o => o.toLowerCase() !== owner))
+        return new Response('Not Found', { status: 404 });
+
     try {
-        const githubResponse = await fetch(`https://api.github.com/repos/${repository}/releases/latest`, {
+        const githubResponse = await fetch(`https://api.github.com/repos/${owner}/${repository}/releases/latest`, {
             headers: {
                 'Accept': 'application/vnd.github+json',
                 'X-GitHub-Api-Version': '2022-11-28',
@@ -15,7 +20,7 @@ export const loader: LoaderFunction = async ({ params }) => {
             return new Response('Not Found', { status: 404 });
 
         const response = json(await githubResponse.json());
-        const cacheControl = githubResponse.headers.get('Cache-Control')?.replace(/s\-maxage=\d+/, `s-maxage=${CACHE_DURATION}`) ?? `s-maxage=${CACHE_DURATION}`;
+        const cacheControl = githubResponse.headers.get('Cache-Control')?.replace(/s-maxage=\d+/, `s-maxage=${CACHE_DURATION}`) ?? `s-maxage=${CACHE_DURATION}`;
         const lastModified = githubResponse.headers.get('Last-Modified');
         response.headers.set('Cache-Control', cacheControl);
         if (lastModified)
