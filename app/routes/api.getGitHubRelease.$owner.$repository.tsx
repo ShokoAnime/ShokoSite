3f7c80dfef308011b613954a587eb183e12375e5
hidden4003 <hidden4003@users.noreply.github.com>
2025-09-16 22:31:58 +0300

    Fix versions not showing (#94)
    * Add more error logging
    
    * Improve response logging
    
    * All requests to Github API must have a User-Agent apparently

--- app/routes/api.getGitHubRelease.$owner.$repository.tsx
+++ app/routes/api.getGitHubRelease.$owner.$repository.tsx
@@ -6,18 +6,28 @@ const OWNERS = ['ShokoAnime', 'Cazzar', 'Mik1ll', 'bigretromike', 'natyusha',];
 export const loader: LoaderFunction = async ({ params }) => {
     const owner = params.owner?.toLowerCase();
     const repository = params.repository?.toLowerCase();
-    if (OWNERS.every(o => o.toLowerCase() !== owner))
-        return new Response('Not Found', { status: 404 });
+    if (OWNERS.every(o => o.toLowerCase() !== owner)) {
+      console.error("Unknown owner:", owner);
+      return new Response('Not Found', {status: 404});
+    }
 
     try {
         const githubResponse = await fetch(`https://api.github.com/repos/${owner}/${repository}/releases/latest`, {
             headers: {
                 'Accept': 'application/vnd.github+json',
                 'X-GitHub-Api-Version': '2022-11-28',
+                'User-Agent': 'request',
             }
         });
-        if (!githubResponse.ok)
-            return new Response('Not Found', { status: 404 });
+        if (!githubResponse.ok) {
+          const text = await githubResponse.text();
+          console.error("Bad github API response:", {
+            status: githubResponse.status,
+            headers: Object.fromEntries(githubResponse.headers),
+            body: text
+          });
+          return new Response('Not Found', {status: 404});
+        }
 
         const response = json(await githubResponse.json());
         const cacheControl = githubResponse.headers.get('Cache-Control')?.replace(/s-maxage=\d+/, `s-maxage=${CACHE_DURATION}`) ?? `s-maxage=${CACHE_DURATION}`;
