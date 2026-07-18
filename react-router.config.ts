54ede0dbde5502c986e8e8b0eb798d3e1ca14eba
User <user@example.com>
2026-07-18 20:48:33 +0300

    Upgrade to react-router  8.2.0

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  future: {
-    v8_middleware: true,
-    v8_splitRouteModules: true,
-    v8_viteEnvironmentApi: true,
-    v8_passThroughRequests: true,
-    v8_trailingSlashAwareDataRequests: true,
-  },
+  splitRouteModules: true,
 } satisfies Config;
